922c73967c5e5ae210c191c64261b4dfdf7b8ccb
ebbay <ebbaydev@gmail.com>
2021-09-18 17:27:49 +0200

    Webdriver Factory added in utility package

--- src/test/java/com/moneygaming/tests/Test1.java
+++ src/test/java/com/moneygaming/tests/Test1.java
@@ -2,7 +2,9 @@ package com.moneygaming.tests;
 
 import com.moneygaming.utilities.WebDriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -10,21 +12,30 @@ import org.testng.annotations.Test;
 
 public class Test1 {
 
+    WebDriver driver;
+
     @BeforeMethod
     public void setUp(){
 
-        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
+        driver.get("https://moneygaming.qa.gameaccount.com/sign-up.shtml");
     }
 
     @Test
-    public void test1(){
+    public void test1() throws InterruptedException {
 
+       WebElement nameBox = driver.findElement(By.cssSelector("#forename"));
+        nameBox.sendKeys("Mike");
+
+        WebElement lastName = driver.findElement(By.xpath("//input[@name='map(lastName)']"));
+        lastName.sendKeys("Smith");
+        Thread.sleep(2000);
     }
 
     @AfterMethod
     public void tearDown(){
-
+driver.quit();
     }
 
 
