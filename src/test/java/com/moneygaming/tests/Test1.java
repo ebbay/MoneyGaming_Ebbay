5afe69e42e717fbac94506e3649429746e583062
ebbay <ebbaydev@gmail.com>
2021-09-18 17:12:44 +0200

    Webdriver Factory added in utility package

--- src/test/java/com/moneygaming/tests/Test1.java
+++ src/test/java/com/moneygaming/tests/Test1.java
@@ -1,5 +1,9 @@
 package com.moneygaming.tests;
 
+import com.moneygaming.utilities.WebDriverFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -9,6 +13,8 @@ public class Test1 {
     @BeforeMethod
     public void setUp(){
 
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
     }
 
     @Test
