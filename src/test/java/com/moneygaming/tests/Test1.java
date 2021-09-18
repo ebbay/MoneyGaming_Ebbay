0e9bc4c3fd8e5845913098b2a3f6420a65ad6382
ebbay <ebbaydev@gmail.com>
2021-09-18 17:00:10 +0200

    My first commit tp my branch

--- src/test/java/com/moneygaming/tests/Test1.java
+++ src/test/java/com/moneygaming/tests/Test1.java
@@ -1,7 +1,27 @@
 package com.moneygaming.tests;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class Test1 {
-    public static void main(String[] args) {
+
+    @BeforeMethod
+    public void setUp(){
 
     }
+
+    @Test
+    public void test1(){
+
+    }
+
+    @AfterMethod
+    public void tearDown(){
+
+    }
+
+
+
+
 }
